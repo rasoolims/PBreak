57899589991691fb6ede1e636e62ea4d8dd9ba94
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-09-30 16:11:07 -0400

    Added first rule (plural breaker)

--- break.py
+++ break.py
@@ -4,12 +4,28 @@ from optparse import OptionParser
 
 from hazm import *
 
+from rules import *
+
 
 def get_lm_option_parser():
     parser = OptionParser()
     parser.add_option("--input", dest="input_path", metavar="FILE", default=None)
     parser.add_option("--output", dest="output_path", metavar="FILE", default=None)
     return parser
+
+
+def tokenize_mwes(words, tags):
+    new_words, new_tags = [], []
+    for word, tag in zip(words, tags):
+        if "_" in word:
+            tokens = word.split("_")
+            for tok in tokens:
+                new_words.append(tok)
+                new_tags.append(tag)
+        else:
+            new_words.append(word.replace(semi_space + semi_space, semi_space))
+            new_tags.append(tag)
+    return new_words, new_tags
 
 
 def word_list2sen(word_list, tags):
@@ -34,7 +50,6 @@ def word_list2sen(word_list, tags):
 
 
 if __name__ == "__main__":
-    all_tags = set()
     parser = get_lm_option_parser()
     (options, args) = parser.parse_args()
     tagger = POSTagger(model='resources/postagger.model')
@@ -49,10 +64,10 @@ if __name__ == "__main__":
             words = word_tokenize(sen)
             tagged_words = tagger.tag(words)
             tags = list(map(lambda x: x[1], tagged_words))
-            all_tags |= set(tags)
-            w.write(word_list2sen(words, tags) + "\n")
+            words, tags = tokenize_mwes(words, tags)
+            broken_words, new_tags = break_words(words, tags)
+            w.write(word_list2sen(broken_words, new_tags) + "\n")
 
             if i % 1000 == 0:
                 print(i, end="\r")
     print("\nFinished")
-    print(all_tags)
---
+++ rules.py
@@ -0,0 +1,44 @@
+import random
+
+# Tags: {'PUNC', 'Ne', 'INT', 'DETe', 'Pe', 'NUM', 'CONJ', 'CONJe', 'RESe', 'CL', 'POSTP', 'PROe', 'V', 'N', 'P', 'ADV', 'PRO', 'DET', 'NUMe', 'RES', 'AJ', 'AJe', 'ADVe'}
+
+
+"""
+Paper reference by Bakhshizadeh and Tabibzadeh (2018): https://bit.ly/30nXgCz
+"""
+
+semi_space = "‌"
+
+
+def plural_breaker(word):
+    """
+    Page 10 of the paper
+    Converts "HA" or "A" for plurals
+    """
+
+    if word.endswith("ها") or word.endswith("های"):
+        ending = ""
+        if word.endswith("های"):
+            ending = "ی"
+            word = word[:-1]
+
+        subword = word[:-2]
+        if subword[-1] == semi_space:
+            subword = subword[:-1]
+        if subword[-1] not in {"ا", "و", "ه"}:
+            word = subword + "ا" + ending
+
+    return word
+
+
+def break_words(words, tags):
+    broken_words = []
+    broken_tags = []
+    for i, (word, tag) in enumerate(zip(words, tags)):
+        if tag in {"N", "Ne"}:
+            if random.random() < 0.5:
+                word = plural_breaker(word)
+
+        broken_words.append(word)
+        broken_tags.append(tag)
+    return broken_words, broken_tags
