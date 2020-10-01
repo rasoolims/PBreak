ea3ee7aff6196484c60d291df5aca5d9c32922f8
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2020-10-01 11:48:43 -0400

    Add normalizer

--- break_words.py
+++ break_words.py
@@ -55,12 +55,12 @@ if __name__ == "__main__":
     parser = get_lm_option_parser()
     (options, args) = parser.parse_args()
     tagger = POSTagger(model='resources/postagger.model')
+    normalizer = Normalizer()
 
     with open(options.input_path, "r") as r, open(options.output_path, "w") as w:
         for i, line in enumerate(r):
             sen = line.strip()
 
-            normalizer = Normalizer()
 
             sen = normalizer.normalize(sen)
             words = word_tokenize(sen)
---
+++ normalize.py
@@ -0,0 +1,16 @@
+from break_words import *
+
+if __name__ == "__main__":
+    parser = get_lm_option_parser()
+    (options, args) = parser.parse_args()
+    tagger = POSTagger(model='resources/postagger.model')
+    normalizer = Normalizer()
+
+    with open(options.input_path, "r") as r, open(options.output_path, "w") as w:
+        for i, line in enumerate(r):
+            sen = line.strip()
+            sen = normalizer.normalize(sen)
+            w.write(sen + "\n")
+            if i % 1000 == 0:
+                print(i, end="\r")
+    print("\nFinished")
